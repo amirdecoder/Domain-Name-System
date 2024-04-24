0f627d4b9121a212304a232685f28fa43e72266b
Amirhossein arabahmadi <121059574+amirdecoder@users.noreply.github.com>
2024-04-25 02:25:11 +0330

    Add files via upload

---
+++ Dns/Documents/Dns.js
@@ -0,0 +1,17 @@
+function Preferred(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    alert('Preferred Dns Server copied 💜');
+}
+
+function Alternate(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    alert('Alternate Dns Server copied 🩵');
+}
