4991198ab6b2b2bb3c6c2675425e88daeb5e724b
Amirhossein arabahmadi <121059574+amirdecoder@users.noreply.github.com>
2024-08-10 18:33:30 +0330

    Add files via upload

---
+++ App/version.js
@@ -0,0 +1,6 @@
+{
+    "version": "1.1.0",
+    "download_url": "https://github.com/yourusername/yourrepo/releases/download/1.0.1/your-software-1.0.1.exe",
+    "changelog": "Added new feature X, improved performance."
+  }
+  
